70422cbdbbdd1a6247d647ab5039618f7e30000a
BOSSe <BOSSe@danielnylander.se>
2026-02-23 08:52:38 +0100

    Add easter egg: 7 clicks on app icon shows confetti message 🎉

--- src/pauskollen/main.py
+++ src/pauskollen/main.py
@@ -110,8 +110,21 @@ class PauskollenWindow(Adw.ApplicationWindow):
         self._current_exercise = None
         self._custom_minutes = 5
 
+        # Easter egg state
+        self._egg_clicks = 0
+        self._egg_timer = None
+
         # Header bar
         header = Adw.HeaderBar()
+        
+        # Add clickable app icon for easter egg
+        app_btn = Gtk.Button()
+        app_btn.set_icon_name(APP_ID)
+        app_btn.add_css_class("flat")
+        app_btn.set_tooltip_text(_("Pauskollen"))
+        app_btn.connect("clicked", self._on_icon_clicked)
+        header.pack_start(app_btn)
+        
         menu_btn = Gtk.MenuButton()
         menu_btn.set_icon_name("open-menu-symbolic")
         menu = Gio.Menu()
@@ -355,6 +368,50 @@ class PauskollenWindow(Adw.ApplicationWindow):
                 self._exercise_progress.set_fraction(elapsed / self._total)
                 return
         self._exercise_progress.set_fraction(1.0)
+
+    def _on_icon_clicked(self, *args):
+        """Handle clicks on app icon for easter egg."""
+        self._egg_clicks += 1
+        if self._egg_timer:
+            GLib.source_remove(self._egg_timer)
+        self._egg_timer = GLib.timeout_add(500, self._reset_egg)
+        if self._egg_clicks >= 7:
+            self._trigger_easter_egg()
+            self._egg_clicks = 0
+
+    def _reset_egg(self):
+        """Reset easter egg click counter."""
+        self._egg_clicks = 0
+        self._egg_timer = None
+        return False
+
+    def _trigger_easter_egg(self):
+        """Show the secret easter egg!"""
+        try:
+            # Play a fun sound
+            import subprocess
+            subprocess.Popen(['paplay', '/usr/share/sounds/freedesktop/stereo/complete.oga'], 
+                           stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        except:
+            # Fallback beep
+            try:
+                subprocess.Popen(['pactl', 'play-sample', 'bell'], 
+                               stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+            except:
+                pass
+
+        # Show confetti message
+        toast = Adw.Toast.new(_("🎉 Du hittade hemligheten!"))
+        toast.set_timeout(3)
+        
+        # Create toast overlay if it doesn't exist
+        if not hasattr(self, '_toast_overlay'):
+            content = self.get_content()
+            self._toast_overlay = Adw.ToastOverlay()
+            self._toast_overlay.set_child(content)
+            self.set_content(self._toast_overlay)
+        
+        self._toast_overlay.add_toast(toast)
 
 
 class PauskollenApp(Adw.Application):
